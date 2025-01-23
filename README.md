3990fa4655dcacd45c5782c37b4ebba73439b9d2
umar <mirzaumar2804@gmail.com>
2025-01-24 01:16:32 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # object-detection-project
+
+
+## How to run:
+  
+
+ '''bash
+conda create -n signlang python=3.7 -y
+ '''
+
+ '''bash
+conda activate signlang 
+ '''
+
+ '''bash
+pip install -r requirements.txt
+ '''
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version = '0.0.0',
+    author = 'Umar',
+    author_email = 'mirzaumar2804@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
