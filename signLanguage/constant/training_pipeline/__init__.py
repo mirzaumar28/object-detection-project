1d44fdf952f340c9857bc917647206e3692a2b77
umar <mirzaumar2804@gmail.com>
2025-02-01 01:51:23 +0530

    data validation and model trainer added

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -2,9 +2,28 @@ import os
 
 ARTIFACTS_DIR : str = "artifacts"
 
+# data ingestion related constants
 
 DATA_INGESTION_DIR_NAME : str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_NAME : str = "feature_store"
 
 DATA_DOWNLOAD_URL : str = "https://github.com/mirzaumar28/random/raw/refs/heads/main/sign_lang.zip"
+
+#  data validation related constants
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "valid", "data.yaml"]
+
+#  model trainer related constants
+
+MODEL_TRAINER_DIR_NAME : str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+
+MODEL_TRAINER_NO_EPOCHS : int = 1
+
+MODEL_TRAINER_BATCH_SIZE : int = 16
