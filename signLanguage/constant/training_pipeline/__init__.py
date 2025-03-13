1762d1a33a49b23f1122efb562b86ae757ac796b
umar <mirzaumar2804@gmail.com>
2025-03-13 22:12:07 +0530

    app added

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -24,6 +24,12 @@ MODEL_TRAINER_DIR_NAME : str = "model_trainer"
 
 MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
 
-MODEL_TRAINER_NO_EPOCHS : int = 1
+MODEL_TRAINER_NO_EPOCHS : int = 300
 
 MODEL_TRAINER_BATCH_SIZE : int = 16
+
+
+#  model pusher related constants
+
+BUCKET_NAME = "sign-lang-2025-umar"
+S3_MODEL_NAME = "best.pt"
